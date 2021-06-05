fba397a695d1538f4c24b946222ff83cca3c8ef8
NakanoNamiki <namiki.nakano.vertex@gmail.com>
2021-06-05 22:49:09 +0900

    add appbar

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,33 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { Appbar } from "react-native-paper";
+import { Platform } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <Appbar.Header>
+      <Appbar.Content title="資格勉強スタディ" />
+      <Appbar.Action icon="pencil" onPress={() => {}} />
+      <Appbar.Action icon="plus" onPress={() => {}} />
+    </Appbar.Header>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
